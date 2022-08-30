7785e5140f2cc632f92c489355113302a0ad80b0
MoWobbler <64741387+MoWobbler@users.noreply.github.com>
2022-08-29 22:55:02 -0400

    Add project files.

---
+++ RelaCoords/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace RelaCoords
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StartBlockTextBox = new System.Windows.Forms.TextBox();
+            this.EndBlockTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.OutputTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(231, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RelaCoords Calculator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(210, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(352, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Convert static coordinates to relative coordinates";
+            // 
+            // StartBlockTextBox
+            // 
+            this.StartBlockTextBox.Location = new System.Drawing.Point(321, 169);
+            this.StartBlockTextBox.Name = "StartBlockTextBox";
+            this.StartBlockTextBox.Size = new System.Drawing.Size(219, 20);
+            this.StartBlockTextBox.TabIndex = 2;
+            this.StartBlockTextBox.DoubleClick += new System.EventHandler(this.StartBlockTextBox_DoubleClick);
+            // 
+            // EndBlockTextBox
+            // 
+            this.EndBlockTextBox.Location = new System.Drawing.Point(321, 196);
+            this.EndBlockTextBox.Name = "EndBlockTextBox";
+            this.EndBlockTextBox.Size = new System.Drawing.Size(219, 20);
+            this.EndBlockTextBox.TabIndex = 3;
+            this.EndBlockTextBox.DoubleClick += new System.EventHandler(this.EndBlockTextBox_DoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(211, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Starting block coords";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(211, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ending block coords";
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(338, 222);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateButton.TabIndex = 6;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(234, 368);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(320, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Note: Calculations are copied directly to your clipboard. Just paste!";
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Enabled = false;
+            this.OutputTextBox.Location = new System.Drawing.Point(313, 256);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(227, 54);
+            this.OutputTextBox.TabIndex = 8;
+            this.OutputTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EndBlockTextBox);
+            this.Controls.Add(this.StartBlockTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.RightToLeftLayout = true;
+            this.Text = "RelaCoords";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox StartBlockTextBox;
+        private System.Windows.Forms.TextBox EndBlockTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox OutputTextBox;
+    }
+}
+
---
+++ RelaCoords/MainForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RelaCoords
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+
+            Regex expression = new Regex(@"[-\d]+");
+
+            string startblock = StartBlockTextBox.Text;
+            string endblock = EndBlockTextBox.Text;
+
+            List<int> startBlockList = new List<int>();
+            List<int> endBlockList = new List<int>(); ;
+
+
+            var results = expression.Matches(startblock);
+
+            if (results.Count != 3)
+            {
+                OutputTextBox.Text = "Something went wrong with the start block text box. Make sure to enter 3 integers separate by spaces there!";
+                return;
+            }
+
+            foreach (Match match in results)
+            {
+                int temp = Int32.Parse(match.Value);
+                startBlockList.Add(temp);
+            }
+
+            var results2 = expression.Matches(endblock);
+
+            if (results2.Count != 3)
+            {
+                OutputTextBox.Text = "Something went wrong with the end block text box. Make sure to enter 3 integers separated by spaces there!";
+                return;
+            }
+
+            foreach (Match match in results2)
+            {
+                int temp = Int32.Parse(match.Value);
+                endBlockList.Add(temp);
+            }
+
+            int finalx = endBlockList[0] - startBlockList[0];
+            int finaly = endBlockList[1] - startBlockList[1];
+            int finalz = endBlockList[2] - startBlockList[2];
+
+            string output = "~" + finalx + " ~" + finaly + " ~" + finalz;
+
+            OutputTextBox.Text = "Copied these bad boys to your clipboard:\n" + output;
+            Clipboard.SetText(output);
+
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartBlockTextBox_DoubleClick(object sender, EventArgs e)
+        {
+            StartBlockTextBox.SelectAll();
+        }
+
+        private void EndBlockTextBox_DoubleClick(object sender, EventArgs e)
+        {
+            EndBlockTextBox.SelectAll();
+        }
+
+    }
+}
